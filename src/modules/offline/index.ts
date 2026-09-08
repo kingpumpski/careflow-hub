dcb14ffd54aad16dbbeb8c2f57b846404a636ae4
Kingsley Norku <32315099+kingpumpski@users.noreply.github.com>
2026-09-08 11:26:30 +0000

    feat: export preauth data provider

--- src/modules/offline/index.ts
+++ src/modules/offline/index.ts
@@ -2,4 +2,5 @@ export * from "./offline-store";
 export * from "./excel-schema";
 export * from "./excel-bridge";
 export * from "./preauth-offline-repository";
+export * from "./preauth-data-provider";
 export * from "./data-mode";
