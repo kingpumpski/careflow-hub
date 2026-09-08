53592f276102799f2dbf96e4a02baed5e3add6e7
Kingsley Norku <32315099+kingpumpski@users.noreply.github.com>
2026-09-08 13:49:05 +0000

    feat: export settlement exception sync

--- src/modules/offline/index.ts
+++ src/modules/offline/index.ts
@@ -4,4 +4,5 @@ export * from "./excel-bridge";
 export * from "./preauth-offline-repository";
 export * from "./preauth-data-provider";
 export * from "./settlement-exception-repository";
+export * from "./settlement-exception-sync";
 export * from "./data-mode";
